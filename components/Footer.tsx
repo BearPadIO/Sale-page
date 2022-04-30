5ec7d73570b35d698d6166d13132dd0cd21dbdec
BearPadIO <101512663+BearPadIO@users.noreply.github.com>
2022-04-30 18:29:52 +0700

    About Risk

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,22 @@
 export default function Footer() {
   return (
-    <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10 text-2xl mb-4">
+    <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10 text-2xl mb-4 items-center flex-col">
       <a
         href='https://linktr.ee/bearpad'
         target='_blank'
         rel='noopener noreferrer'
       >
         Linktree{" "}
+        
+      </a>
+      
+      <a
+        href='https://www.docdroid.net/1dAaEdz/disclaimer-pdf'
+        target='_blank'
+        rel='noopener noreferrer'
+        className="font-bold"
+      >
+        About Risk{" "}
         
       </a>
     </footer>
