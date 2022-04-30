187fc503bab21941990285aea966d5d7de4e1c73
BearPadIO <101512663+BearPadIO@users.noreply.github.com>
2022-04-30 18:15:29 +0700

    /

--- lib/products.ts
+++ lib/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 'tokens',
     name: 'Seed round',
-    description: 'The minimum purchase size is 134 BP',
+    description: 'The minimum purchase size is 134 BP, maximum size 6700 BP',
     unitName: 'BP', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.0005,
     priceUsd: 0.075,
